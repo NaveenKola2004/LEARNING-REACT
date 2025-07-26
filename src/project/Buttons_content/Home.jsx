8182b3e6dcc37efbda69d6eb1e3c261428dd35b1
NAVEEN <kolanaveen797@gmail.com>
2025-07-26 20:36:18 +0530

    style added

--- src/project/Buttons_content/Home.jsx
+++ src/project/Buttons_content/Home.jsx
@@ -3,7 +3,7 @@ export function Home(){
     return(
         <>
         <div>
-            <h1>👋 Hey there! I'm Tony</h1>
+            <h1 className="eduhed">👋 Hey there! I'm Tony</h1>
             <p>I'm a full-time learner, part-time debugger, and a full-stack explorer on a mission to turn coffee into code.
 Whether it's crafting clean frontends with React + Vite or building solid backends with Node.js, Express, Flask, MySQL, and MongoDB, I love creating smart, efficient solutions that just work. Currently, I’m sharpening my skills for placement season and working on cool projects like a Stock Management System and Student Management APIs.</p>
                    <li>💡 What I’m building</li>
@@ -11,8 +11,8 @@ Whether it's crafting clean frontends with React + Vite or building solid backen
                    <li>🛠️ Tips, bugs, and fixes I encounter</li>
                    <li>🎯 My journey from student to software developer</li>
         <div>
-            <img src="./src/project/PHOTOS/HOME_PHOTOS/photo1.jpg" alt="some photos here"></img>
-            <img src="./src/project/PHOTOS/HOME_PHOTOS/photo2.jpg" alt="some photos here"></img>
+            <img src="./src/project/PHOTOS/HOME_PHOTOS/photo1.jpg" alt="some photos here" className="images"></img>
+            <img src="./src/project/PHOTOS/HOME_PHOTOS/photo2.jpg" alt="some photos here" className="images"></img>
         </div>
                     </div>
         </>
